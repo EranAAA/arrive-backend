6bf4d4cd3740426137e9aab1035bc84f646ce2a3
Eran Avichzer <eranavichzer@Erans-Mac-mini.lan>
2022-08-21 19:27:17 +0300

    fixed sql connection

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.use('/api/trip', tripRoutes)
 
 // Last fallback
 app.get('/**', (req, res) => {
-   // res.sendFile(path.join(__dirname, 'public', 'index.html'))
+   res.sendFile(path.join(__dirname, 'public', 'index.html'))
 })
 
 const port = process.env.PORT || 3030
